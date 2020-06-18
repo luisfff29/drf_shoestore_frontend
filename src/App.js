32ae48dcde28be6ad950badb5a93ea96bf73c14d
luisfff29 <luisfff29@gmail.com>
2020-06-18 13:26:29 -0500

    display instances on a single page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { shoes: [] };
+  }
+  componentDidMount() {
+    fetch("http://localhost:8000/api/shoe/")
+      .then((res) => res.json())
+      .then((data) => this.setState({ shoes: data }));
+  }
+  render() {
+    return (
+      <div style={{ margin: "2em" }}>
+        <h1>Shoe Store</h1>
+        {this.state.shoes.map((shoe) => {
+          return (
+            <ul>
+              <li>
+                <b>SIZE:</b> {shoe.size}
+              </li>
+              <li>
+                <b>BRAND:</b> {shoe.brand}
+              </li>
+              <li>
+                <b>MANUFACTURER:</b> {shoe.manufacturer}
+              </li>
+              <li>
+                <b>COLOR:</b> {shoe.color}
+              </li>
+              <li>
+                <b>MATERIAL:</b> {shoe.material}
+              </li>
+              <li>
+                <b>STYLE:</b> {shoe.shoe_type}
+              </li>
+              <li>
+                <b>FASTEN:</b> {shoe.fasten_type}
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
